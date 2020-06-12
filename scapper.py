c770f96bdb080dc4f9b577ff9a016197fa9cff97
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-12 08:46:50 -0500

    added comments

--- scapper.py
+++ scapper.py
@@ -4,15 +4,19 @@ import requests as r
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+# Request data from desired url
 request = r.get('https://www.akc.org/expert-advice/lifestyle/35-perfect-pictures-of-dogs/')
 soup = BeautifulSoup(request.text, "html.parser")
 
+# source the images link which is to be downloaded
 x = soup.select('img[src^="http://cdn.akc.org/content/article-body-image/"]')
 
+# generate links from the which the images are to be downloaded
 links = []
 for img in x:
     links.append(img['src'])
 
+# Create directory where the downloaded images are to be written
 path = 'photos'
 isDir = os.path.isdir(path)
 if isDir:
@@ -21,9 +25,11 @@ else:
     print('Creating a directory\n')
     os.mkdir('photos')
 
+# Retrieves date time in the format index_month_date_year_hours:minutes:seconds_am/pm
 now = datetime.now()
 dt_string = now.strftime("%B_%d_%Y_%I:%M:%S_%p")
 
+# Generate and save only up to 10 images
 i = 1
 for index, img_link in enumerate(links):
     if i <= 10:
