17cec63321d320fdc533996c743bef63636ca715
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-12 08:55:46 -0500

    added exception handler

--- insta.py
+++ insta.py
@@ -5,15 +5,18 @@ from bs4 import BeautifulSoup as bs
 def extract_dp():
     url = f'https://www.instagram.com/{user_name}/'
     print(f'Connecting to profile {url}')
-    response = urllib.request.urlopen(url).read()
-    soup = bs(response, 'html.parser')
-    meta = soup.find_all('meta', {'property': 'og:image'})
-    img_src = meta[0]['content']
-    # imgURL = imgURL.replace('s150x150', 's720x720')
-    name = url.split('/')[-2]
-    img_name = f'{name}.jpg'
-    urllib.request.urlretrieve(img_src, img_name)
-    print(f'Downloaded image has been saved as {img_name}')
+    try:
+        response = urllib.request.urlopen(url).read()
+        soup = bs(response, 'html.parser')
+        meta = soup.find_all('meta', {'property': 'og:image'})
+        img_src = meta[0]['content']
+        # imgURL = imgURL.replace('s150x150', 's720x720')
+        name = url.split('/')[-2]
+        img_name = f'{name}.jpg'
+        urllib.request.urlretrieve(img_src, img_name)
+        print(f'Downloaded image has been saved as {img_name}')
+    except:
+        print(f"Unable to connect to '{user_name}'. Make sure the username is correct.")
 
 
 if __name__ == '__main__':
